1b113857f588db45ef9c18ae107a821149d13d84
jimmy omar <jimmy@gmail.com>
2022-10-25 23:33:43 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoAppMovil/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProyectoAppMovil
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+
+            string usuario = "1";
+            string contraseña = "1";
+            string tUsuario = txtUsuario.Text;
+            string tContraseña = txtContraseña.Text;
+            if (usuario == tUsuario & contraseña == tContraseña)
+            {
+                DisplayAlert("Bitacora para Conductores", "Bienvenido" + usuario, "Cerrar");
+                Navigation.PushAsync(new Menu());
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Usuario No existe!!", "Cerrar");
+            }
+
+
+        }
+    }
+}
